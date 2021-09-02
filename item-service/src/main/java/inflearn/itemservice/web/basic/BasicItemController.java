071be55d8af54c2d18402f075943fc5369704d34
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2021-09-03 00:39:39 +0900

    feat: Apply thymeleaf
    feat: Apply thymeleaf

---
+++ item-service/src/main/java/inflearn/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,43 @@
+package inflearn.itemservice.web.basic;
+
+
+import inflearn.itemservice.domain.item.Item;
+import inflearn.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository){
+//        this.itemRepository=itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
